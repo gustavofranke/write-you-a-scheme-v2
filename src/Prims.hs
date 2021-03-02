bbaa8cd00e4a81f46bd892a3484ca9f7c66da84e
Gustavo Franke <franke.gustavo@gmail.com>
2021-03-02 21:04:34 +0000

    prim env, add impls

--- src/Prims.hs
+++ src/Prims.hs
@@ -3,12 +3,12 @@
 module Prims where
 
 import Control.Exception
-import qualified Data.Text as T
-import LispVal
 import Control.Monad
 import Control.Monad.Except
-import System.IO
+import qualified Data.Text as T
 import qualified Data.Text.IO as TIO
+import LispVal
+import System.IO
 
 type Prim = [(T.Text, LispVal)]
 
@@ -20,8 +20,8 @@ mkF :: ([LispVal] -> Eval LispVal) -> LispVal
 mkF = Fun . IFunc
 
 primEnv :: Prim
-primEnv = [
-    ("+", mkF $ binopFold (numOp (+)) (Number 0)),
+primEnv =
+  [ ("+", mkF $ binopFold (numOp (+)) (Number 0)),
     ("*", mkF $ binopFold (numOp (*)) (Number 1)),
     ("++", mkF $ binopFold (strOp (<>)) (String "")),
     ("-", mkF $ binop $ numOp (-)),
@@ -50,83 +50,89 @@ unop op [x] = op x
 unop _ args = throw $ NumArgs 1 args
 
 binop :: Binary -> [LispVal] -> Eval LispVal
-binop op [x,y] = op  x y
+binop op [x, y] = op x y
 binop _ args = throw $ NumArgs 2 args
 
 binopFold :: Binary -> LispVal -> [LispVal] -> Eval LispVal
 binopFold op farg args = case args of
-  [a,b] -> op a b
-  (a:as) -> foldM op farg args
+  [a, b] -> op a b
+  (a : as) -> foldM op farg args
   [] -> throw $ NumArgs 2 args
 
 strOp :: (T.Text -> T.Text -> T.Text) -> LispVal -> LispVal -> Eval LispVal
 strOp op (String x) (String y) = return $ String $ op x y
-strOp op x          (String y) = undefined
-strOp op (String x) y = undefined
-strOp op x          y = undefined 
+strOp op x (String y) = throw $ TypeMismatch "string op " x
+strOp op (String x) y = throw $ TypeMismatch "string op " y
+strOp op x y = throw $ TypeMismatch "string op " x
 
 fileExists :: LispVal -> Eval LispVal
-fileExists (Atom atom) = undefined
-fileExists (String txt) = undefined
+fileExists (Atom atom) = fileExists $ String atom
+fileExists (String txt) = Bool <$> liftIO (doesFileExist $ T.unpack txt)
 fileExists val = throw $ TypeMismatch "expects str, got: " val
+
+doesFileExist :: String -> IO Bool
+doesFileExist = error "not implemented"
 
 slurp :: LispVal -> Eval LispVal
 slurp (String txt) = liftIO $ wFileSlurp txt
 slurp val = throw $ TypeMismatch "expects str, got: " val
 
-wFileSlurp :: T.Text -> IO LispVal 
+wFileSlurp :: T.Text -> IO LispVal
 wFileSlurp fileName = withFile (T.unpack fileName) ReadMode go
   where
     go = readTextFile fileName
 
-readTextFile :: T.Text -> Handle -> IO LispVal 
+readTextFile :: T.Text -> Handle -> IO LispVal
 readTextFile fileName handle = do
   exists <- hIsEOF handle
   if exists
-  then (TIO.hGetContents handle) >>= (return . String)
-  else throw $ IOError $ T.concat [" file does not exist: ", fileName]
+    then (TIO.hGetContents handle) >>= (return . String)
+    else throw $ IOError $ T.concat [" file does not exist: ", fileName]
 
-cons :: [LispVal]  -> Eval LispVal
-cons [x, y@(List yList)] = return $ List $ x:y:yList
+cons :: [LispVal] -> Eval LispVal
+cons [x, y@(List yList)] = return $ List $ x : y : yList
 cons [c] = return $ List [c]
 cons [] = return $ List []
 cons _ = throw $ ExpectedList "cons, in second argument"
 
 car :: [LispVal] -> Eval LispVal
 car [List []] = return Nil
-car [List (x:_)] = undefined
-car [] = undefined
-car x = undefined
+car [List (x : _)] = return x
+car [] = return Nil
+car x = throw $ ExpectedList "car"
 
 cdr :: [LispVal] -> Eval LispVal
-cdr [List (x:xs)] = undefined
-cdr [List []] = undefined 
-cdr [] = undefined 
-cdr x = undefined
+cdr [List (x : xs)] = return $ List xs
+cdr [List []] = return Nil
+cdr [] = return Nil
+cdr x = throw $ ExpectedList "cdr"
 
-numBool :: (Integer -> Bool) -> LispVal -> Eval LispVal 
-numBool op (Number x) = undefined 
-numBool op x = undefined
+numBool :: (Integer -> Bool) -> LispVal -> Eval LispVal
+numBool op (Number x) = return $ Bool $ op x
+numBool op x = throw $ TypeMismatch "numeric op" x
 
 numOp :: (Integer -> Integer -> Integer) -> LispVal -> LispVal -> Eval LispVal
-numOp = undefined
+numOp op (Number x) (Number y) = return $ Number $ op x y
+numOp op x (Number y) = throw $ TypeMismatch "numeric op" x
+numOp op (Number x) y = throw $ TypeMismatch "numeric op" y
+numOp op x y = throw $ TypeMismatch "numeric op" x
 
 eqOp :: (Bool -> Bool -> Bool) -> LispVal -> LispVal -> Eval LispVal
 eqOp op (Bool x) (Bool y) = return $ Bool $ op x y
-eqOp op x (Bool y) = undefined
-eqOp op (Bool x) y = undefined
-eqOp op x y = undefined
+eqOp op x (Bool y) = throw $ TypeMismatch "bool op" x
+eqOp op (Bool x) y = throw $ TypeMismatch "bool op" y
+eqOp op x y = throw $ TypeMismatch "bool op" x
 
 numCmp :: (Integer -> Integer -> Bool) -> LispVal -> LispVal -> Eval LispVal
-numCmp op (Number x) (Number y) = undefined
-numCmp op x (Number y) = undefined
-numCmp op (Number x) y = undefined
-numCmp op x y = undefined
+numCmp op (Number x) (Number y) = return . Bool $ op x y
+numCmp op x (Number y) = throw $ TypeMismatch "numeric op" x
+numCmp op (Number x) y = throw $ TypeMismatch "numeric op" y
+numCmp op x y = throw $ TypeMismatch "numeric op" x
 
 eqCmd :: LispVal -> LispVal -> Eval LispVal
-eqCmd (Atom x) (Atom y)= undefined
-eqCmd (Number x) (Number y) = undefined
-eqCmd (String x) (String y) = undefined
-eqCmd (Bool x) (Bool y) = undefined
-eqCmd Nil Nil = undefined
-eqCmd _ _ = undefined
+eqCmd (Atom x) (Atom y) = return . Bool $ x == y
+eqCmd (Number x) (Number y) = return . Bool $ x == y
+eqCmd (String x) (String y) = return . Bool $ x == y
+eqCmd (Bool x) (Bool y) = return . Bool $ x == y
+eqCmd Nil Nil = return $ Bool True
+eqCmd _ _ = return $ Bool False
