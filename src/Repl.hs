83797aee018a112b9442f7c7f6e65cfb87bf59d0
Gustavo Franke <franke.gustavo@gmail.com>
2021-03-03 20:44:40 +0000

    start work on repl

--- src/Repl.hs
+++ src/Repl.hs
@@ -1,2 +1,21 @@
 module Repl where
-    
+
+import Eval
+import Data.Text as T
+
+import Control.Monad.Trans
+import System.Console.Haskeline
+
+type Repl a = InputT IO a
+
+mainLoop :: IO ()
+mainLoop = runInputT defaultSettings repl
+
+repl :: Repl ()
+repl = undefined
+
+process :: String -> IO ()
+process = undefined
+
+processToAST :: String -> IO ()
+processToAST = undefined
